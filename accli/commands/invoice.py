3d378a3a01507059c62dac0d45f63f090dfcc519
José Luis Segura Lucas <jsegura@blue-tc.com>
2017-02-02 12:10:42 +0100

    Added 'generate' option to 'invoice' command and configuring the subparser

--- accli/commands/invoice.py
+++ accli/commands/invoice.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8; mode python -*-
 
+import os
+
+import accli.config as config
 from accli.core import Command
 
 
@@ -32,9 +35,43 @@ class ShowCmd(Command):
         return 0
 
 
+class GenerateCmd(Command):
+    def __init__(self, subparsers):
+        super().__init__('generate', subparsers)
+        self.parser.add_argument(
+            'template_name',
+            help=("Name of the template to use. Use <name> of "
+                  "'<template-dir>/<name>'"))
+        self.parser.add_argument(
+            'invoice_paths', nargs='+', default=[],
+            help='Paths to input invoice')
+        self.parser.add_argument(
+            '-d', '--data-dir', dest='data_dir',
+            default=config.ACCLI_DATA_ROOTDIR,
+            help='set the path to accli data root directory')
+        self.parser.add_argument(
+            '-t', '--template-dir', dest='template_dir',
+            default='templates/mkinvoice',
+            help=("set the path to template root directory. "
+                  "Default 'templates/mkinvoice'"))
+        self.parser.add_argument(
+            '-o', '--output-dir', dest='output_dir', default=os.getcwd(),
+            help='set the output path for the generated files')
+        self.parser.add_argument(
+            '-f', '--format', dest='format', default='tex', choices=['tex'],
+            help="defines template format. Default 'tex'")
+        self.parser.add_argument(
+            '-r', '--list-renders', dest='list_renders', action='store_true',
+            help='shows available renders.')
+
+    def run(self, args):
+        print(args)
+        return 0
+
+
 class InvoiceCmd(Command):
 
-    commands = [ListCmd, ShowCmd]
+    commands = [ListCmd, ShowCmd, GenerateCmd]
 
     def __init__(self, subparsers):
         super().__init__('invoice', subparsers)
